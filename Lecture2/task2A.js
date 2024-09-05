439111e15527b2e55bee2e9fc0d98bc0b807d758
Fatima-malik7 <fatimamalikk77@gmail.com>
2024-09-05 11:42:57 +0500

    Add Lecture2/ to Lecture-practice branch

---
+++ Lecture2/task2A.js
@@ -0,0 +1,15 @@
+// Define functions
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Export multiple functions
+  module.exports = {
+    add,
+    subtract
+  };
+  
