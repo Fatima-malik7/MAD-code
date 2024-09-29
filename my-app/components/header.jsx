5c32dd5ac1fb55179165e81aae6ae3de39193561
Fatima-malik7 <fatimamalikk77@gmail.com>
2024-09-29 22:10:30 +0500

    Assignment 1

---
+++ my-app/components/header.jsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+const Header=(props)=>
+{
+    return
+    {
+        <>
+        <View>
+            <Text>Hello! This is fatima's android app</Text>
+        </View>
+        </>
+        
+    }
+}
+export default Header;
